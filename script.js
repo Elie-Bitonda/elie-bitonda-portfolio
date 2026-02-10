36f889bdbad42b5197289d642dc734e5e2bfb8a1
Elie-Bitonda <eliedanger12@gmail.com>
2026-02-10 16:48:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const revealOnScroll = () => {
   const windowHeight = window.innerHeight;
 
   reveals.forEach(section => {
-    const sectionTop = section.getBoundingClientRect().top;
-    if (sectionTop < windowHeight - 100) {
+    const top = section.getBoundingClientRect().top;
+    if (top < windowHeight - 120) {
       section.classList.add("active");
     }
   });
